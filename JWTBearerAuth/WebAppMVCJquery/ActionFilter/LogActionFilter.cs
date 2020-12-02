783fc5108969dd47a005e12e3f9216e2fcc74287
san3205san <48003300+san3205san@users.noreply.github.com>
2020-12-02 23:31:09 +0530

    kendo page
    with kendo

---
+++ JWTBearerAuth/WebAppMVCJquery/ActionFilter/LogActionFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Diagnostics;
+
+namespace WebAppMVCJquery.ActionFilter
+{
+    public class LogActionFilter : ActionFilterAttribute
+
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Debug.WriteLine("OnActionExecuting");
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Debug.WriteLine("OnActionExecuted");
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Debug.WriteLine("OnResultExecuting");
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Debug.WriteLine("OnResultExecuted");
+        }
+
+
+        private void Log(string methodName, RouteData routeData)
+        {
+            var controllerName = routeData.Values["controller"];
+            var actionName = routeData.Values["action"];
+            var message = String.Format("{0} controller:{1} action:{2}", methodName, controllerName, actionName);
+            Debug.WriteLine(message, "Action Filter Log");
+        }
+
+    }
+}
--- JWTBearerAuth/WebAppMVCJquery/Controllers/HomeController.cs
+++ JWTBearerAuth/WebAppMVCJquery/Controllers/HomeController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebAppMVCJquery.ActionFilter;
 using WebAppMVCJquery.Models;
 
 namespace WebAppMVCJquery.Controllers
 {
+    [LogActionFilter]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -25,13 +28,47 @@ namespace WebAppMVCJquery.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
+
+            var list = new List<CheckModel>
+            {
+                 new CheckModel{Id = 1, Name = "Aquafina", Checked = false},
+                 new CheckModel{Id = 2, Name = "Mulshi Springs", Checked = false},
+                 new CheckModel{Id = 3, Name = "Alfa Blue", Checked = false},
+                 new CheckModel{Id = 4, Name = "Atlas Premium", Checked = false},
+                 new CheckModel{Id = 5, Name = "Bailley", Checked = false},
+                 new CheckModel{Id = 6, Name = "Bisleri", Checked = false},
+                 new CheckModel{Id = 7, Name = "Himalayan", Checked = false},
+                 new CheckModel{Id = 8, Name = "Cool Valley", Checked = false},
+                 new CheckModel{Id = 9, Name = "Dew Drops", Checked = false},
+                 new CheckModel{Id = 10, Name = "Dislaren", Checked = false},
+
+            };
+            return View(list);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public ActionResult Index(IFormCollection fc, CheckModel m)
+        {
+            
+            foreach(var item in fc.ToList())
+            {
+                //       if(item==true)
+                //    {
+
+                //    }
+                //    else
+                //    {
+
+                //    }
+
+            }
+                return View();
         }
     }
 }
